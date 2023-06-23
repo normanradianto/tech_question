a4938420e1ee96eefc5268c3f992ad613aba595b
normanradianto-kororo <norman.radianto@kororo.co>
2023-06-23 17:08:33 +0700

    adding test.txt

--- database/seeders/ArticleTableSequenceSeeder.php
+++ database/seeders/ArticleTableSequenceSeeder.php
@@ -13,6 +13,6 @@ class ArticleTableSequenceSeeder extends Seeder
      */
     public function run(): void
     {
-        Article::factory()->count(3)->sequence(['title' => 'Test Article', 'body' => 'Test Article', 'content_type' => 'Article', 'published' => 'Yes'], ['title' => 'Test Article Two', 'body' => 'Test Article Two', 'content_type' => 'Article', 'published' => 'Yes'], ['title' => 'Test Article Three', 'body' => 'Test Article Three', 'content_type' => 'Article', 'published' => 'Yes'])->create();
+        \App\Models\Article::factory()->count(3)->sequence(['title' => 'Test Article', 'body' => 'Test Article', 'content_type' => 'Article', 'published' => 'Yes'], ['title' => 'Test Article Two', 'body' => 'Test Article Two', 'content_type' => 'Article', 'published' => 'Yes'], ['title' => 'Test Article Three', 'body' => 'Test Article Three', 'content_type' => 'Article', 'published' => 'Yes'])->make();
     }
 }
